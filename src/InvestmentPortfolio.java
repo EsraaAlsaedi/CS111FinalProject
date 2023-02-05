b674e1f5e2e36df359ec2994f77916141b3ddae7
esra <esra@esras-mbp.lan>
2023-02-05 22:11:37 +0300

    CS111ProjectWarmUp

--- src/InvestmentPortfolio.java
+++ src/InvestmentPortfolio.java
@@ -9,12 +9,25 @@ public class InvestmentPortfolio {
 
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        
+        //here variables for user 1 
         
         String name2=read.next();
         String id2=read.next();
         int aaplShare2=read.nextInt();
         int mfstShare2=read.nextInt();
         int googlShare2=read.nextInt();
+        int serviceID;
+        
+        System.out.println("Welcome to Max Profit Stock Exchange");
+        do {
+         System.out.println("To show equity summary, please enters 1.");
+         System.out.println("To show stock portfilo, please enters 2.");
+         System.out.println("To exit the program, please enters 0");
+         serviceID=read.nextInt();
+             //put switch here
+         
+         
+         
+        } while(serviceID!=0 & (serviceID>2 | serviceID<0) );
     }
 }
